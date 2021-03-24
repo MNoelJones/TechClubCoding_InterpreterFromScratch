ef80c43a1dc1ce6e3bf5a74d33d9d38dbadb4eb3
MNJones <mnjones@gmail.com>
2021-03-24 17:35:58 +0000

    Added test for assignment 3

---
+++ tests/test_assignment3.py
@@ -0,0 +1,28 @@
+import hy
+import interpreter
+import pytest
+
+
+@pytest.mark.parametrize(
+    "program, expected",
+    (
+        (
+            (
+                "Apple Pear Banana // Comment on the morality of fruit as a pizza topping. "
+                "Or do something else with your time"
+            ),
+            "applepearbanana"
+        ),
+    )
+)
+def test_assignment_3(program, expected):
+    assert interpreter.interpreter(program) == expected
+
+
+def test_assignment_3_2():
+    program = "This is quite a long sentence, // Is it not?"
+    assert pytest.raises(
+        interpreter.InvalidCharacter,
+        interpreter.interpreter,
+        program
+    )
